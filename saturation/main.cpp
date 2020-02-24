7cde4e66fc64c6084f157419f78ef82fd9daf3e8
edtorrez <etorrez2017@gmail.com>
2020-02-24 16:59:40 -0500

    terrible but it works

--- saturation/main.cpp
+++ saturation/main.cpp
@@ -1,5 +1,8 @@
+// srry for bad code but I didn't want to over engineer
+
 #include <iostream>
 #include <math.h>
+#include <stdio.h>
 
 #define PI 3.14159265
 
@@ -9,35 +12,183 @@ float calculateSaturation( int red, int green, int blue, float intensity );
 int calculateHue( int red, int green, int blue );
 int minimum( int red, int green, int blue );
 int maximum( int red, int green, int blue );
+void toPixel( int& hue, float& saturation, float& intensity );
+void writeToFile( FILE* file, unsigned char& data, int size );
+void average();
 
 void HSI_to_RGB( float& red, float& green, float& blue, int hue, float saturation, float intensity );
+void RGB_to_HSI( float red, float green, float blue, int& hue, float& saturation, float& intensity );
+
+//size of the image
+#define H 1920
+#define W 1080
+
+#define DATASIZE H*W*3
+
+unsigned char data[DATASIZE];
+
+unsigned char hueData[DATASIZE];
+unsigned char satData[DATASIZE];
+unsigned char intData[DATASIZE];
+
+unsigned char avgHue[H*W];
+unsigned char avgSat[H*W];
+unsigned char avgInt[H*W];
+
+FILE* src_file;
+FILE* out_file;
+FILE* hue_file;
 
 int main()
 {
-    // original rgb values
-    int red = 254;
-    int green = 123;
-    int blue = 132;
+    src_file = fopen( "gta_1.raw", "rb" );
+    fread( data, 1, DATASIZE, src_file );
+    fclose( src_file );
     
-    float intensity = calculateIntensity( red, green, blue );
-    float saturation = calculateSaturation( red, green, blue, intensity );
-    int hue = calculateHue( red, green, blue );
+    int x = 0;
+    for( int i = 0; i < DATASIZE; i = i + 3 )
+    {   
+        int red = data[i];
+        int green = data[i+1];
+        int blue = data[i+2];
+        
+        int hue;
+        float saturation;
+        float intensity;
 
-    std::cout << "RGB: " << red << " " << green << " " << blue << std::endl;
-    std::cout << "HUE: " << hue << std::endl;
-    std::cout << "SAT: " << saturation << std::endl;
-    std::cout << "INT: " << intensity << std::endl;
+        RGB_to_HSI( red, green, blue, hue, saturation, intensity );
+        
+        avgHue[x] = hue;
+        avgSat[x] = saturation;
+        avgInt[x] = intensity;
+        x++;
+
+        toPixel( hue, saturation, intensity );
+        
+        hueData[i] = hue;
+        hueData[i+1] = hue;
+        hueData[i+2] = hue;
+        
+        satData[i] = saturation;
+        satData[i+1] = saturation;
+        satData[i+2] = saturation;
+        
+        intData[i] = intensity;
+        intData[i+1] = intensity;
+        intData[i+2] = intensity;
+    }
     
-    float tred = 0;
-    float tgreen = 0;
-    float tblue = 0;
+    average();
 
-    HSI_to_RGB( tred, tgreen, tblue, hue, saturation, intensity );
+    out_file = fopen( "gta_1_hue.raw", "wb" );
+    fwrite( hueData, 1, DATASIZE, out_file );
+    fclose( out_file );
+    
+    out_file = fopen( "gta_1_sat.raw", "wb" );
+    fwrite( satData, 1, DATASIZE, out_file );
+    fclose( out_file );
+    
+    out_file = fopen( "gta_1_int.raw", "wb" );
+    fwrite( intData, 1, DATASIZE, out_file );
+    fclose( out_file );
+    
 
-    std::cout << std::endl << "RGB: " << tred << " " << tgreen << " " << tblue << std::endl;
+
+    src_file = fopen( "gta_2.raw", "rb" );
+    fread( data, 1, DATASIZE, src_file );
+    fclose( src_file );
+    
+    x = 0;
+    for( int i = 0; i < DATASIZE; i = i + 3 )
+    {   
+        int red = data[i];
+        int green = data[i+1];
+        int blue = data[i+2];
+        
+        int hue;
+        float saturation;
+        float intensity;
+
+        RGB_to_HSI( red, green, blue, hue, saturation, intensity );
+        
+        avgHue[x] = hue;
+        avgSat[x] = saturation;
+        avgInt[x] = intensity;
+        x++;
+        
+        toPixel( hue, saturation, intensity );
+
+        hueData[i] = hue;
+        hueData[i+1] = hue;
+        hueData[i+2] = hue;
+        
+        satData[i] = saturation;
+        satData[i+1] = saturation;
+        satData[i+2] = saturation;
+        
+        intData[i] = intensity;
+        intData[i+1] = intensity;
+        intData[i+2] = intensity;
+    }
+    
+    average();
+    
+    out_file = fopen( "gta_2_hue.raw", "wb" );
+    fwrite( hueData, 1, DATASIZE, out_file );
+    fclose( out_file );
+    
+    out_file = fopen( "gta_2_sat.raw", "wb" );
+    fwrite( satData, 1, DATASIZE, out_file );
+    fclose( out_file );
+    
+    out_file = fopen( "gta_2_int.raw", "wb" );
+    fwrite( intData, 1, DATASIZE, out_file );
+    fclose( out_file );
+
     return 0;
 }
 
+
+void average()
+{
+    float avg; 
+    int size = H*W;
+    
+    avg = 0;
+    for( int i = 0; i < size; i++ )
+    {
+        avg = avg + avgHue[i];
+    }
+    std::cout << "HUE: " << int(avg/size);
+
+    avg = 0;
+    for( int i = 0; i < size; i++ )
+    {
+        avg = avg + avgSat[i];
+    }
+    std::cout << "  SAT: " << avg/size;
+
+    avg = 0;
+    for( int i = 0; i < size; i++ )
+    {
+        avg = avg + avgInt[i];
+    }
+    std::cout << "  INT: " << avg/size << std::endl;
+}
+
+/**
+ * converts RGB to HSI
+*/
+void RGB_to_HSI( float red, float green, float blue, int& hue, float& saturation, float& intensity )
+{
+    intensity = calculateIntensity( red, green, blue );
+    saturation = calculateSaturation( red, green, blue, intensity );
+    hue = calculateHue( red, green, blue );
+}
+
+/**
+ * converts HSI to RGB
+*/
 void HSI_to_RGB( float& red, float& green, float& blue, int hue, float saturation, float intensity )
 {   
     float x = intensity*(1.0 - saturation);
@@ -69,11 +220,26 @@ void HSI_to_RGB( float& red, float& green, float& blue, int hue, float saturatio
     blue = round(blue * 255);
 }
 
+void toPixel( int& hue, float& saturation, float& intensity )
+{
+    float temp = hue/360.0;
+    
+    hue = temp * 255.0;
+    saturation = int(saturation * 255);
+    intensity = int(intensity * 255);
+}
+
+/**
+ * calculates intensity of rgb value 0-255
+*/
 float calculateIntensity( int red, int green, int blue )
 {
     return (red + green + blue) / (3.0*255);
 }
 
+/**
+ * calculates saturation of rgb value 0-255
+*/
 float calculateSaturation( int red, int green, int blue, float intensity )
 {
     int min = minimum( red, green, blue );
@@ -82,6 +248,9 @@ float calculateSaturation( int red, int green, int blue, float intensity )
     return (intensity - normal_min) / intensity;
 }
 
+/**
+ * calculates the hue of rgb value 0-255
+*/
 int calculateHue( int red, int green, int blue )
 {
     // find max and min
@@ -92,7 +261,6 @@ int calculateHue( int red, int green, int blue )
     // avoid division by 0
     if( min == max )
     {
-        std::cout << "min and max are the same!" << std::endl;
         return 0;
     }
     
@@ -113,6 +281,9 @@ int calculateHue( int red, int green, int blue )
     return hue;
 }
 
+/**
+ * returns minimum value
+*/
 int minimum( int red, int green, int blue )
 {
     int minValue = red;
@@ -126,6 +297,9 @@ int minimum( int red, int green, int blue )
     return minValue;
 }
 
+/**
+ * returns maximum value
+*/
 int maximum( int red, int green, int blue )
 {
     int maxValue = red;
@@ -139,6 +313,9 @@ int maximum( int red, int green, int blue )
     return maxValue;
 }
 
+/**
+ * converts from 0-255 to 0-1
+*/
 float normalize( int color )
 {
     return color / 255.0;
